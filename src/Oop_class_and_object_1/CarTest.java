14327574ba9f1c0ac8cf664871e81e3c9d74c87a
Learta <leartaistrefaj03@gmail.com>
2021-10-08 11:58:27 +0200

    Dorezimi i detyrave - OOP

--- src/Oop_class_and_object_1/CarTest.java
+++ src/Oop_class_and_object_1/CarTest.java
@@ -1,5 +1,7 @@
 package Oop_class_and_object_1;
 
+import com.sun.nio.sctp.SctpSocketOption;
+
 public class CarTest {
     public static void main(String[] args) {
         Car c1 = new Car();
@@ -7,6 +9,14 @@ public class CarTest {
         c1.setTipi("M5");
         c1.setNgjyra("E bardhe");
         c1.setVitiProdhimit(2012);
+
+        Car c2 = new Car();
+        c2.setNgjyra("E kuqe");
+        c2.setBrendi("Mercedes");
+        c2.setTipi("S Class");
+        c2.setVitiProdhimit(2017);
+
+        System.out.println("Vetura e pare eshte nga brendi " + c1.getBrendi() + " dhe ka ngjyren " + c1.getNgjyra());
 
     }
 }
