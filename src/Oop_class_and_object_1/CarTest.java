73571a930cb3d95196a0870d5b95380b84b1b7f4
Learta <leartaistrefaj03@gmail.com>
2021-10-08 11:08:17 +0200

    Dorezimi i detyrave - OOP

--- src/Oop_class_and_object_1/CarTest.java
+++ src/Oop_class_and_object_1/CarTest.java
@@ -3,6 +3,10 @@ package Oop_class_and_object_1;
 public class CarTest {
     public static void main(String[] args) {
         Car c1 = new Car();
+        c1.setBrendi("BMW");
+        c1.setTipi("M5");
+        c1.setNgjyra("E bardhe");
+        c1.setVitiProdhimit(2012);
 
     }
 }
